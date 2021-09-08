c1b815c245f09a1b51b2bad92dade246aea81635
Billy Bryant <brybi@amazon.com>
2021-09-08 14:52:33 -0700

    Updating Keyboard Code & Keymap

--- firmware/metalboards/insanemacropad/keymaps/default/keymap.c
+++ firmware/metalboards/insanemacropad/keymaps/default/keymap.c
@@ -1,29 +1,13 @@
 #include QMK_KEYBOARD_H
 
-enum custom_keycodes {
-  UP_URL = SAFE_RANGE
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT_ortho_4x4(
-        KC_7, KC_8,   KC_9,   KC_ASTR,
-        KC_4, KC_5,   KC_6,   KC_SLSH,
-        KC_1, KC_2,   KC_3,   KC_MINS,
-        KC_0, KC_ENT, KC_DOT, KC_EQL
+        LCA(KC_F13), LCA(KC_F14), LCA(KC_F15), LCA(KC_F16),
+        LCA(KC_F17), LCA(KC_F18), LCA(KC_F19), LCA(KC_F20),
+        LSA(KC_F13), LSA(KC_F14), LSA(KC_F15), LSA(KC_F16),
+        LSA(KC_F17), LSA(KC_F18), LSA(KC_F19), LSA(KC_F20)
     )
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case UP_URL:
-            if (record->event.pressed) {
-                SEND_STRING("http://1upkeyboards.com");
-            }
-            return false;
-            break;
-    }
-    return true;
-}
 
 #ifdef ENCODER_ENABLE
 #include "encoder.h"
@@ -33,6 +17,12 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
       tap_code(KC_VOLU);
     } else {
       tap_code(KC_VOLD);
+    }
+  } else if (index == 1) {
+    if (clockwise) {
+	    tap_code(KC_MFFD);
+    } else {
+	    tap_code(KC_MRWD);
     }
   }
   return true;
--- firmware/metalboards/insanemacropad/v1/promicro/config.h
+++ firmware/metalboards/insanemacropad/v1/promicro/config.h
@@ -3,29 +3,24 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define PRODUCT_ID      0x8191
+#define PRODUCT_ID      0x8091
 #define DEVICE_VER      0x0001
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { D4, D1, E6, B5 }
-#define MATRIX_COL_PINS { F7, F6, D2, D3 }
+#define MATRIX_COL_PINS { D1, D0, D4, C6 }
+#define MATRIX_ROW_PINS { D7, E6, B4, B5 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION ROW2COL
+#define DIODE_DIRECTION COL2ROW
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
 /* Encoder pins */
-#define ENCODERS_PAD_A { F4 }
-#define ENCODERS_PAD_B { F5 }
+#define ENCODERS_PAD_A { B1, B2 }
+#define ENCODERS_PAD_B { B3, B6 }
 #define ENCODER_RESOLUTION 4
+#define ENCODER_DIRECTION_FLIP
 
-/* Underglow options */
-#define RGB_DI_PIN F4
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 1
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
+
